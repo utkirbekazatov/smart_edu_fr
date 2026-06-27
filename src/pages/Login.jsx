abbc3d14a8d5b1fb22adac332ae53896f128ebc0
azatovutkirbek <azatovutkirbek6@gmail.com>
2026-06-27 22:08:55 +0500

    ffff

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import api from './api'; // api.js faylingizdan import qilamiz
+import api from '../api'; // api.js faylingizdan import qilamiz
 import { GraduationCap, User, Lock, ArrowLeft, ShieldCheck, UserCheck, Briefcase, Loader2 } from 'lucide-react';
 
 function Login() {
