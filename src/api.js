494356d006b43cb4b824ada602f0757e1200c3a1
azatovutkirbek <azatovutkirbek6@gmail.com>
2026-06-28 15:23:15 +0500

    jjjj

--- src/api.js
+++ src/api.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({
+export default axios.create({
     baseURL: "https://smarteduproject.fwh.is/api/",
-    withCredentials: false,
     headers: {
         Accept: "application/json",
-        "Content-Type": "application/json",
-    },
+        "Content-Type": "application/json"
+    }
 });
-
-export default api;
